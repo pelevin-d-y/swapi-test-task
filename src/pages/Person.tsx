9086d57b232bd30df61595af91044845f67bafaa
Denis Pelevin <pelevin.d.y@gmail.com>
2023-05-02 14:37:50 +0700

    feat: add data grid for person

--- src/pages/Person.tsx
+++ src/pages/Person.tsx
@@ -1,9 +1,16 @@
-import { Box, Paper, Stack, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { useEffect, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchPerson, selectPerson } from '../features/person/personSlice'
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 import { Loader } from '../components/Loader/Loader'
+import { DataGrid } from '@mui/x-data-grid'
+
+type Row = {
+  id: string
+  property_name: string
+  property_value: string | string[]
+}
 
 export const Person = () => {
   const { id } = useParams()
@@ -14,12 +21,31 @@ export const Person = () => {
     dispatch(fetchPerson(id!))
   }, [id])
 
-  const personItems = useMemo(() => {
-    if (!person) return []
-    return Object.entries(person).reduce<Array<string | string[]>[]>(
-      (acc, [key, value]) => (acc = [...acc, [key, value]]),
-      []
-    )
+  const gridData = useMemo(() => {
+    if (!person) return null
+
+    const columns = [
+      { field: 'property_name', headerName: 'Property name', width: 250 },
+      {
+        field: 'property_value',
+        headerName: 'Property value',
+        width: 300,
+        editable: true,
+      },
+    ]
+
+    const rows = Object.entries(person).reduce<Row[]>((acc, [key, value]) => {
+      if (Array.isArray(value)) return acc
+
+      let newValue =
+        key === 'created' || key === 'edited'
+          ? new Date(value).toLocaleString()
+          : value
+
+      return [...acc, { id: key, property_name: key, property_value: newValue }]
+    }, [])
+
+    return { columns, rows }
   }, [person])
 
   return (
@@ -29,20 +55,8 @@ export const Person = () => {
           <Typography variant="h1" gutterBottom>
             {person?.name}
           </Typography>
-          <Box>
-            {person && (
-              <Paper variant="outlined" elevation={4} sx={{ p: 2 }}>
-                <Stack spacing={2} direction="column" alignItems="center">
-                  {personItems.map(([key, value]) => (
-                    <Stack width="100%" direction="row">
-                      <Box sx={{ minWidth: 200 }}>{key}:</Box>
-                      <Box>{value}</Box>
-                    </Stack>
-                  ))}
-                </Stack>
-              </Paper>
-            )}
-          </Box>
+
+          {gridData ? <DataGrid {...gridData} /> : null}
         </>
       ) : (
         <Loader />
